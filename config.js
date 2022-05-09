6431df29b2cca99c2a0631ab60a2fbd4672af94f
7827 <84583344+7827@users.noreply.github.com>
2022-05-09 15:36:59 +0530

    Add files via upload

---
+++ config.js
@@ -0,0 +1,17 @@
+import firebase from "firebase";
+require("@firebase/firestore");
+
+//Paste your firebaseConfig here
+const firebaseConfig = {
+    apiKey: "AIzaSyBZrEeU2GVbn8uH378kPf1XV3VaVO565bw",
+    authDomain: "pro-71-877f3.firebaseapp.com",
+    databaseURL: "https://pro-71-877f3-default-rtdb.firebaseio.com",
+    projectId: "pro-71-877f3",
+    storageBucket: "pro-71-877f3.appspot.com",
+    messagingSenderId: "37432734953",
+    appId: "1:37432734953:web:1f5cadda261bce9856ec78"
+  };
+//
+firebase.initializeApp(firebaseConfig);
+
+export default firebase.firestore();
